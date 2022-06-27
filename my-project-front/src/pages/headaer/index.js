36853f4dc19756a14478b290ebbd67df5261df99
tiank <tiank@deepblueai.com>
2022-06-27 23:19:51 +0800

    添加我的弹出框

--- my-project-front/src/pages/headaer/index.js
+++ my-project-front/src/pages/headaer/index.js
@@ -1,9 +1,21 @@
 import './index.css'
+import {useState} from "react";
+import tuichu from './img/a-26wode3.svg';
+import gerenzhongxin from './img/a-30shouye2.svg';
 
 const Index =()=> {
+    const [showPopOfMy,setShowPopOfMy] = useState(false);
     // 我的头像的点击事件
     const toMy = ()=> {
-        console.log('tomyyyy')
+        setShowPopOfMy(!showPopOfMy)
+    }
+    const renderPopOfMy = ()=> {
+        return (
+            <ul className='popOfMy'>
+                <li className='popItem'><a href="http://www.tiankaii.cn"><img src={gerenzhongxin} alt=""/>我的主页</a></li>
+                <li className='popItem'><a href="/"><img src={tuichu} alt=""/>退出</a></li>
+            </ul>
+        )
     }
 
     return (
@@ -12,6 +24,7 @@ const Index =()=> {
                 <li className='toHome'>首页</li>
                 <li className='toMy' onClick={toMy} title='我的主页'></li>
             </ul>
+            {showPopOfMy ? renderPopOfMy() :null}
         </div>
 
     );
